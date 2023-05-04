a99e92980c39a897bd0b1ca96b1b1952ea8aa55e
Josean_Borges <132008054+JoseanBorges@users.noreply.github.com>
2023-05-04 17:06:43 -0300

    Adição da atividade
    Com funções

---
+++ Atividade.py
@@ -0,0 +1,9 @@
+from Funcoes import *
+
+num1=int(input('Digite o primeiro numero: '))
+num2=int(input('Digite o segundo numero: '))
+print ('O resultado da soma é: ', soma(num1,num2))
+print ('O resultado da multiplicação é: ', multiplicacao(num1,num2))
+print ('O resultado da divisão é: ', divisao (num1,num2))
+print ('O resultado da subtração é: ', subtracao(num1,num2))
+print('\nMuito obrigado que você tenha um excelente dia!!!')
